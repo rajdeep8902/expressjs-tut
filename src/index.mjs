6936649983a0c9e98306bbc2ecd33835647e9baf
Rajdeep <rajdeepbiswasndp@gmail.com>
2025-01-18 12:28:28 +0530

    cookies

--- src/index.mjs
+++ src/index.mjs
@@ -1,12 +1,17 @@
 import express from 'express';
 import indexRouter from './routes/index.mjs';
+import cookieParser from 'cookie-parser';
+
 const app = express();
+
 app.use(express.json());
-app.use(indexRouter)
+app.use(cookieParser('helloworld'));
+app.use(indexRouter);
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
+    res.cookie('hello', 'world', { maxAge: 60000, signed: true })
     res.status(201).send({ msg: "hello" })
 })
 app.listen(PORT, (req, res) => {
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -1,11 +1,17 @@
 import { Router } from "express";
-const prodRouter=Router();
+const prodRouter = Router();
 
 prodRouter.get('/api/products', (req, res) => {
-    res.send([
-        { prod_id: 1, username: "mobile" },
-        { prod_id: 2, username: "laptop" }
-    ])
+    console.log(req.headers.cookie);
+    console.log(req.cookies);
+    console.log(req.signedCookies)
+    if (req.signedCookies.hello && req.signedCookies.hello === 'world') {
+        return res.send([
+            { prod_id: 1, username: "mobile" },
+            { prod_id: 2, username: "laptop" }
+        ])
+    }
+    return res.status(403).send({ msg: "you need correct cookie" })
 })
 
 export default prodRouter;
